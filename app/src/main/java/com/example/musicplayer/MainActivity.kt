f2fd571ecfebc4bab330d33ca6ef08adc7dc69ba
chenna <chennareddyannapureddy@gmail.com>
2021-09-08 17:02:36 +0530

    added permissions in AndroidManifest.xml changed ext.kotlin_version to 1.4.32 changed gradle_version 4.1.3 added two functions in MainActivity.kt

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,14 +1,43 @@
 package com.example.musicplayer
 
+import android.Manifest
+import android.app.Activity
+import android.app.DownloadManager
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
-    val name : Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        requestPermission()
+
+    }
+
+    private fun hasPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+    }
+
+    private fun requestPermission() {
+        var permission = mutableListOf<String>()
+
+        if (!hasPermission()) {
+            permission.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        if (permission.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permission.toTypedArray(), 8)
+        }
     }
 }
