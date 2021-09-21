bcf5ce6cf4b647c88859f8b819cd09bd01f225b2
Shubhamm Pednekar <67822091+shubyaa@users.noreply.github.com>
2021-09-21 19:32:41 +0530

    Rearranged Code

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.musicplayer
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.database.Cursor
 import android.media.AudioAttributes
@@ -15,6 +16,7 @@ import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.musicplayer.Model.SongModel
@@ -286,31 +288,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun deleteMenuOption() {
-       /* val file = File(list[position].songUri.toString())
         val dialogBuilder = AlertDialog.Builder(this)
+
         try {
             dialogBuilder.setMessage("Are you sure you want to delete ${list[position].name}?")
                 .setCancelable(false)
                 .setPositiveButton("Yes", DialogInterface.OnClickListener { dialogInterface, i ->
-                    file.delete()
-                    nextPrevious(true)
+//WE HAVE TO ADD SOMETHING TO DELETE
                 })
                 .setNegativeButton(
-                    "No",
-                    DialogInterface.OnClickListener { dialogInterface, i -> dialogInterface.cancel() })
+                    "No"
+                ) { dialogInterface, i -> dialogInterface.cancel() }
 
             val alert = dialogBuilder.create()
 
             alert.setTitle("Delete File?")
-
             alert.show()
-        }catch (e: java.lang.Exception){
-            Log.i("exception at delete",e.toString())
+
+        } catch (e: java.lang.Exception) {
+            Log.i("exception at delete", e.toString())
         }
-
-        */
-
-        Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show()
     }
 
     private fun adjustSeekBar(seekBar: SeekBar) {
@@ -324,11 +321,11 @@ class MainActivity : AppCompatActivity() {
                 nextPrevious(true)
             }
 
-            handler.postDelayed(runnable, 100)
+            handler.postDelayed(runnable, 300)
 
         }
 
-        handler.postDelayed(runnable, 100)
+        handler.postDelayed(runnable, 300)
     }
 
     private fun getAlbumArt(uri: Uri): ByteArray? {
